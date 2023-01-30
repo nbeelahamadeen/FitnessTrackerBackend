2f151ae6c7cbd0865de204910897c832943beb90
Kristyvdm2020 <kristyvdm2020@gmail.com>
2023-01-30 12:41:43 -0500

    added functions createActivity and getAllActivities

--- db/activities.js
+++ db/activities.js
@@ -3,10 +3,30 @@ const client = require('./client');
 // database functions
 async function createActivity({ name, description }) {
   // return the new activity
+  try {
+    const {rows: [activity]} = await client.query(`
+    INSERT INTO activities(name, description)
+    VALUES ($1, $2)
+    RETURNING *`, 
+    [name, description]);
+    
+    return activity;
+  } catch(err) {
+    throw err;
+  }
 }
 
 async function getAllActivities() {
   // select and return an array of all activities
+  try {
+    const { rows } = await client.query(`
+    SELECT * FROM activities;
+    `);
+
+    return rows;
+  } catch (err) {
+    throw err;
+  }
 }
 
 async function getActivityById(id) {}
