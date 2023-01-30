c2ebf27c39c3a7852c12073b9832f677dd9d89ee
nbeelahamadeen <nbeelahamadeen@gmail.com>
2023-01-30 11:36:00 -0600

    completed user.js file

--- db/users.js
+++ db/users.js
@@ -4,24 +4,43 @@ const client = require("./client");
 
 // user functions
 async function createUser({ username, password }) {
-  // try{
-  //   const { rows: [ user ] }
-  // } catch(error){
-  //   throw error;
-  // } 
+  const { rows: [ user ] } = await client.query(`
+  INSERT INTO users(username, password)
+  VALUES($1, $2)
+  ON CONFLICT (username) DO NOTHING
+  RETURNING *;
+  `, [username, password]);
+  return user;
+
 }
 
 async function getUser({ username, password }) {
-
+  const { rows } = await client.query(`
+  SELECT id, username, password
+  FROM users;
+  `, [username, password]);
+  return rows;
 }
 
 async function getUserById(userId) {
-
+  const { row: [user] } = await client.query(`
+  SELECT id, username, password
+  FROM users
+  WHERE id=${userId}
+  `)
+  return user;
 }
 
-async function getUserByUsername(userName) {
+async function getUserByUsername(username) {
+  const { rows: [user] } = await client.query(`
+  SELECT *
+  FROM users
+  WHERE username=$1;
+  `, [username]);
 
+  return user;
 }
+
 
 module.exports = {
   createUser,
