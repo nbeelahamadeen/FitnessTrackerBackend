8a217314662814f7f437e28ce9b0108563e4245b
Kristyvdm2020 <kristyvdm2020@gmail.com>
2023-01-30 11:54:45 -0500

    added tables for routines and routine_activities;

--- db/seedData.js
+++ db/seedData.js
@@ -8,6 +8,8 @@ async function dropTables() {
   // drop all tables, in the correct order
   try{
     await client.query(`
+    DROP TABLE IF EXISTS routine_activities;
+    DROP TABLE IF EXISTS routines;
     DROP TABLE IF EXISTS activities;
     DROP TABLE IF EXISTS users;
     `);
@@ -35,6 +37,23 @@ async function createTables() {
       id SERIAL PRIMARY KEY, 
       name VARCHAR(255) UNIQUE NOT NULL, 
       description TEXT NOT NULL
+    );
+
+    CREATE TABLE routines (
+      id SERIAL PRIMARY KEY, 
+      "creatorId" INTEGER REFERENCES users(id),
+      "isPublic" BOOLEAN DEFAULT false, 
+      name VARCHAR(255) UNIQUE NOT NULL, 
+      goal TEXT NOT NULL
+    );
+
+    CREATE TABLE routine_activities (
+      id SERIAL PRIMARY KEY, 
+      "routineId" INTEGER REFERENCES routines(id),
+      "activityId" INTEGER REFERENCES activities(id),
+      duration INTEGER,
+      count INTEGER, 
+      UNIQUE ("routineId", "activityId")
     );
     `)
   } catch(error){
