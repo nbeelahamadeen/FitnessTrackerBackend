7caf0f7f719ea8f2aaded2629cf707864d76de35
Kristyvdm2020 <kristyvdm2020@gmail.com>
2023-02-06 16:26:43 -0500

    added route for registering users and tests passed

--- api/users.js
+++ api/users.js
@@ -1,8 +1,57 @@
 /* eslint-disable no-useless-catch */
 const express = require("express");
-const router = express.Router();
+const userRouter = express.Router();
+const jwt = require('jsonwebtoken');
+const {
+    UserTakenError,
+    PasswordTooShortError,
+    UnauthorizedError,
+  } = require("../errors");
+
+const { createUser, getUserByUsername } = require('../db');
 
 // POST /api/users/register
+userRouter.post('/register', async (req, res, next) => {
+    const { username, password } = req.body;
+
+    try {
+        const duplicateUser = await getUserByUsername(username);
+
+        if(duplicateUser) {
+            res.send({
+                "error": "UserTakenError",
+                "message": UserTakenError(username),
+                "name": username
+            });
+        }
+
+        if(password.length < 8) {
+            res.send({
+                "error": 'PasswordTooShort',
+                "message": PasswordTooShortError(), 
+                "name": username
+            })
+        }
+        
+        const user = await createUser({username, password});
+
+
+        const token = jwt.sign({
+            id: user.id,
+            username}, 
+            process.env.JWT_SECRET
+        );
+
+        res.send({
+            "message": "Thank you for signing up", 
+            "token": token,
+            "user": user
+        });
+         
+    } catch (error) {
+      next(error); 
+    }
+})
 
 // POST /api/users/login
 
@@ -10,4 +59,4 @@ const router = express.Router();
 
 // GET /api/users/:username/routines
 
-module.exports = router;
+module.exports = userRouter;
