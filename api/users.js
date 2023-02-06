642b618441beef17acd62a69f73689a6c96cffb8
Kristyvdm2020 <kristyvdm2020@gmail.com>
2023-02-06 17:30:11 -0500

    started on users route for get api/users/me

--- api/users.js
+++ api/users.js
@@ -2,13 +2,15 @@
 const express = require("express");
 const userRouter = express.Router();
 const jwt = require('jsonwebtoken');
+const { JWT_SECRET } = process.env;
+
 const {
     UserTakenError,
     PasswordTooShortError,
     UnauthorizedError,
   } = require("../errors");
 
-const { createUser, getUserByUsername, getUser } = require('../db');
+const { createUser, getUserByUsername, getUser, getUserById } = require('../db');
 const client = require("../db/client");
 
 // POST /api/users/register
@@ -80,8 +82,37 @@ userRouter.post('/login', async (req, res, next) =>{
         next(error);
     }
 })
-// GET /api/users/me
 
+// GET /api/users/me
+userRouter.get('/me', async (req, res, next) => {
+    const prefix = 'Bearer ';
+    const auth = req.header('Authorization');
+    if(!auth) {
+        next();
+    } else if (auth.startsWith(prefix)) {
+        const token = auth.slice(prefix.length);
+    
+    try {
+        const { id } = jwt.verify(token, JWT_SECRET);
+
+        if(id) {
+            const user = await getUserById(id);
+            res.send(user);
+        } else {
+            //unable to get 401 status without valid token
+            res.send({
+                "message": UnauthorizedError(),
+                "error": "UnauthorizedError", 
+                "name": "UnauthorizedError"
+            });
+            return res.status(401);
+        }
+
+    } catch (error) {
+        next(error);
+    }
+}
+})
 // GET /api/users/:username/routines
 
 module.exports = userRouter;
